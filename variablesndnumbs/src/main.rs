845d7a14a288ba0c40f400d857241eda2fefba99
kushagra2503 <yashyyy831@gmail.com>
2025-07-27 15:24:37 +0530

    hehehhe

--- variablesndnumbs/src/main.rs
+++ variablesndnumbs/src/main.rs
@@ -1,8 +1,21 @@
 fn main() {
-    let x: i32 = 5; // Signed 32-bit integer
+    let x: i32 = 5; // Signed 32-bit integer use i8 if number is smaller and in range from -128 to 127
     let y: u32 = 10; // Unsigned 32-bit integer cannot be negative
     let z: f64 = 2.718; // 64-bit floating point number
 
     print!("x: {}, y: {}, z: {}", x, y, z); // Print variables without newline  
     println!(); // Print a newline at the end
+
+    let is_male: bool = false; // Boolean variable, can be true or false
+    let is_above_18: bool = true;
+
+    if is_male {
+        println!("You are male");
+    } else {
+        println!("You are gay");
+    }
+
+    if is_male && is_above_18 {
+        println!("You are not gay"); //&& operator checks if both conditions are true
+    }
 }
